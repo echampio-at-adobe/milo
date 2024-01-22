25c1ce2118d0fa9920f6667f47f2e58d10010ee2
Erich Champion <echampio@adobe.com>
2024-01-22 12:06:14 -0800

    different try at authentication

--- libs/blocks/quiz/utils.js
+++ libs/blocks/quiz/utils.js
@@ -487,7 +487,8 @@ async function clearCache(url, key, value, baseUrl) {
               method: 'POST',
               headers: {
                   'Content-Type': 'application/x-www-form-urlencoded',
-                  'credentials': "include",
+                  'authority: admin.hlx.page',
+                  'credentials': "same-origin",
                   // Add any additional headers as needed
               },
               // Add any request body data if required
